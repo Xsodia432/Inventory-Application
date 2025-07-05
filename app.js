288af71c6b76b6b194910dacadb8b0ea0c07939f
clarence <reyannpahinado@gmail.com>
2025-07-06 03:57:00 +0800

    Installed dependencies and create folders

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
